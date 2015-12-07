e48c1fcfd7a123794077d7b848d2d7f895c0905b
Joffrey Mann <Joffrmann25@gmail.com>
2015-12-07 09:54:43 -0500

    Initial Commit

---
+++ Fibonacci/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fibonacci
+//
+//  Created by Joffrey Mann on 12/7/15.
+//  Copyright © 2015 Joffrey Mann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Fibonacci/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fibonacci
+//
+//  Created by Joffrey Mann on 12/7/15.
+//  Copyright © 2015 Joffrey Mann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
